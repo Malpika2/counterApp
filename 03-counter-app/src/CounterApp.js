25ca3a4679bf78319826a39e58b18330df9cfa55
Anthony <hamthony@hotmail.com>
2021-02-24 16:56:11 -0600

    test

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+export default function  CounterApp({value = 10}) {
+
+    const [counter, setCounter] = useState(value)
+    const handleAdd = (e) => {
+        // setCounter(counter +1 );
+        setCounter( (c) => c+1 );
+
+    }
+    const handleReset =  () => {
+        setCounter(value);
+    }
+    const handleSub = () => {
+        setCounter(counter-1);
+    }
+    return (
+    <>
+        <h1>CounterApp</h1>
+        <h2> {counter} </h2>
+        <button onClick={ handleAdd }> + 1</button>
+        <button onClick={ handleReset }> Reset </button>
+        <button onClick={ handleSub }> - 1</button>
+    </>) ;
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
